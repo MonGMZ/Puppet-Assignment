54fd44fb0ed53a17603c451f4935e873c12decf9
MonGMZ <jose.ramon.gonzalez.martinez@gmail.com>
2020-02-16 11:32:12 +0100

    nginx1 ip 10.10.10.10

--- reverse/puppet/manifests/reverse3.pp
+++ reverse/puppet/manifests/reverse3.pp
@@ -8,19 +8,23 @@ class{'nginx':
     package_source => 'nginx-stable'
 }
 nginx::resource::server { 'devops.local':
-  proxy       => 'https://10.0.0.101',
+  proxy               => 'https://10.10.10.10',
   ssl                 => true,
   ssl_cert            => '/home/vagrant/nginx_conf/cert.crt',
   ssl_key             => '/home/vagrant/nginx_conf/cert.key',
+  # location_cfg_append => {
+  #   health_check        =>  'interval=10 passes=2 fails=3'
+  # }
 }
 
 nginx::resource::location { "/resource2":
-  ensure          => present,
-  ssl             => true,
-  proxy           => 'https://20.20.20.20/',
-  ssl_only        => true,
-  server          => "devops.local",
-  location_cfg_append => {
+  ensure                    => present,
+  ssl                       => true,
+  proxy                     => 'https://20.20.20.20/',
+  ssl_only                  => true,
+  server                    => "devops.local",
+  location_cfg_append       => {
+    #health_check            => 'interval=10 passes=2 fails=3',
     fastcgi_connect_timeout => '3m',
     fastcgi_read_timeout    => '3m',
     fastcgi_send_timeout    => '3m'
